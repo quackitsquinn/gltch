5b3d96f7b6d01b5319e7c7acbe6a31e075a1954a
quackitsquinn <quinnweismiller@gmail.com>
2025-10-25 16:54:03 -0500

    Use latest iced commit instead of crates.io version

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use gltch::GlitchApp;
 
 fn main() -> iced::Result {
     env_logger::init();
-    iced::run("GLXTCH", GlitchApp::update, GlitchApp::view)
+    iced::application(GlitchApp::default, GlitchApp::update, GlitchApp::view).run()
 }
