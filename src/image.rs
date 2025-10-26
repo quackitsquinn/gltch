7eb7ec81e771c3479140f05623a9ae9aa83545f0
quackitsquinn <quinnweismiller@gmail.com>
2025-10-25 23:47:42 -0500

    Use Arcs for image data

--- src/image.rs
+++ src/image.rs
@@ -1,18 +1,24 @@
 //! Image handling for Glitch.
-use std::path::{Path, PathBuf};
+use std::{
+    path::{Path, PathBuf},
+    sync::Arc,
+};
 
+use bytes::Bytes;
 use iced::widget::image::Handle;
-use image::{ColorType, GenericImageView, RgbaImage};
+use image::{ColorType, GenericImageView, ImageBuffer, Rgba, RgbaImage};
 use log::{error, info};
+
+/// Type alias for an image buffer used in Glitch.
+pub type GlitchImageBuffer = ImageBuffer<Rgba<u8>, Arc<[u8]>>;
 
 /// A struct representing an image loaded in Glitch.
 #[derive(Debug)]
-
 pub struct GlitchImage {
     /// The file path of the image.
     pub file: PathBuf,
     /// The image data.
-    pub image: image::RgbaImage,
+    pub image: GlitchImageBuffer,
     /// The Iced image handle. This will likely be refactored out later to replace the redundant cloning of image data.
     pub handle: Option<Handle>,
 }
@@ -23,25 +29,28 @@ impl GlitchImage {
         let file = file.as_ref().to_path_buf();
         let img = image::open(&file)?;
         let (width, height) = img.dimensions();
+
         info!(
             "Opening {} with dimensions ({}x{})",
             file.display(),
             width,
             height
         );
-        if img.color() != ColorType::Rgba8 {
-            error!(
-                "Image at {:?} is not in RGBA8 format, found {:?} instead. Downsampling...",
-                file,
-                img.color()
-            );
-        }
+
         let img = img.to_rgba8();
-        // This is gross but iced's image handle API is limited. This might mark as another reason to just use wgpu directly.
-        let handle = Handle::from_rgba(width, height, img.as_raw().clone());
+
+        let image_data = img.as_raw().clone();
+        let arc: Arc<[u8]> = Arc::from(image_data);
+        // First, create a new ImageBuffer that uses Arc for its pixel data.
+        // This allows us to share the pixel data without unnecessary copies.
+        let image_buf: GlitchImageBuffer =
+            ImageBuffer::from_raw(width, height, arc.clone()).expect("failed to convert to arc");
+        // Now, create the Iced Handle from the raw RGBA data. Thankfully, Bytes::from_owned exists and supports Arc.
+        let handle = Self::make_handle(&image_buf);
+
         Ok(GlitchImage {
             file,
-            image: img,
+            image: image_buf,
             handle: Some(handle),
         })
     }
@@ -52,7 +61,7 @@ impl GlitchImage {
     }
 
     /// Updates the image data and refreshes the Iced image handle.
-    pub fn update_with(&mut self, img: RgbaImage) {
+    pub fn update_with(&mut self, img: GlitchImageBuffer) {
         let (width, height) = img.dimensions();
         info!(
             "Updating image {} with new dimensions ({}x{})",
@@ -60,8 +69,14 @@ impl GlitchImage {
             width,
             height
         );
+
         self.image = img;
-        let handle = Handle::from_rgba(width, height, self.image.as_raw().clone());
-        self.handle = Some(handle);
+        self.handle = Some(Self::make_handle(&self.image));
+    }
+
+    fn make_handle(image: &GlitchImageBuffer) -> Handle {
+        let (width, height) = image.dimensions();
+        let image_data = image.as_raw().clone();
+        Handle::from_rgba(width, height, Bytes::from_owner(image_data))
     }
 }
